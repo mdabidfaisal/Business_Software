8edb4e6b3d37a10ed4e68ec70cf450505c4d5602
Md. Abid Faisal <128960092+mdabidfaisal@users.noreply.github.com>
2024-02-01 04:20:29 +0600

    first

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Sidebar Start //
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
