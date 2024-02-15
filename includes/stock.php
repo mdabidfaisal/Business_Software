a6cdb57ac3dbffe6861c822e8367800bce08c926
abidfaisal54 <159251724+abidfaisal54@users.noreply.github.com>
2024-02-15 09:36:50 +0000

    ADAF

--- includes/stock.php
+++ includes/stock.php
@@ -67,7 +67,7 @@
                 </div>
                 <div class="col-md-3 pt-3">
                     <select class="form-select" aria-label="Default select example">
-                        <option selected>Brandy</option>
+                        <option selected>Brand</option>
                         <option value="1">One</option>
                         <option value="2">Two</option>
                         <option value="3">Three</option>
